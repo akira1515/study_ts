d735a2201a2db1a8fabe3655c5e162acca38b311
akira1515 <akira.kobayashi1515@gmail.com>
2023-10-03 15:25:36 +0900

    型とアノテーション

---
+++ src/basic/any.ts
@@ -0,0 +1,6 @@
+export default function anySample() {
+  // let name: any = '虎ハック'
+  // console.log('any sample 1:', typeof name, name)
+  // name = 28
+  // console.log('any sample 2:', typeof name, name)
+}
